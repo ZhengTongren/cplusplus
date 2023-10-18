058623b43f0c15054e1bffa2162707bd59d324de
tianhuo-tongren <1811334612@qq.com>
2023-10-18 19:12:47 +0800

    23_10_18

--- test_10_18/test_10_18/Test.cpp
+++ test_10_18/test_10_18/Test.cpp
@@ -4,20 +4,39 @@
 #include <iostream>
 
 using namespace std;
+//
+//
+//void Func(int a, int b = 20, int c = 30)
+//{
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl << endl;
+//}
+//
+//
+//int main()
+//{
+//	Func(1);
+//	Func(1, 2);
+//	Func(1, 2, 3);
+//	return 0;
+//}
 
 
-void Func(int a, int b = 20, int c = 30)
+int* Func(int& y)
 {
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
-	cout << "c = " << c << endl << endl;
+	return &y;
 }
-
 
 int main()
 {
-	Func(1);
-	Func(1, 2);
-	Func(1, 2, 3);
+	int a = 10;
+	int c = 20;
+	int* p = &a;
+	cout << "p = " << p << endl;
+	p = Func(c);
+	cout << "&c = " << &c << endl;
+	cout << "p = " << p << endl;
+
 	return 0;
 }
