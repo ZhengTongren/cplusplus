0e14265869737412d4ee7a107c8389af652aa1a2
tianhuo-tongren <1811334612@qq.com>
2023-10-19 10:40:39 +0800

    23_10_19

---
+++ test_10_19/test_10_19/Test.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <iostream>
+using namespace std;
+
+
+//int& Add(int x, int y)
+//{
+//	static int c = x + y;
+//	return c;
+//}
+
+
+//int& Add(int x, int y)
+//{
+//	static int c;
+//	c = x + y;
+//	return c;
+//}
+//
+//
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	cout << ret << endl;
+//
+//	Add(2, 4);
+//	cout << ret << endl;
+//
+//	return 0;
+//}
+
+
