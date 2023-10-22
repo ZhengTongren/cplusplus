326fbdb5f18fb18ff3c1ffdfca30877cbd3d89dc
tianhuo-tongren <1811334612@qq.com>
2023-10-22 11:18:53 +0800

    23_10_22

---
+++ test_10_22/test_10_22/Test.cpp
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	int a = 10;
+//	// 引用必须初始化
+//	// int& b;
+//	int& b = a;
+//
+//	int c = 20;
+//	// 引用不可改变对象
+//	// 一旦引用一个实体，就不可引用其他实体
+//	b = c;
+//
+//
+//	return 0;
+//}
+
+
+//int Count()
+//{
+//	int n = 0;
+//	n++;
+//	return n;
+//}
+
+
+//int& Count()
+//{
+//	int n = 0;
+//	n++;
+//	return n;
+//}
+//
+//
+//int main()
+//{
+//	//int ret = Count();
+//
+//	int& ret = Count();
+//
+//	cout << ret << endl;
+//
+//	cout << ret << endl;
+//
+//	Count();
+//	cout << 
+//
+//	return 0;
+//}
+
+
+//int& Add(int x, int y)
+//{
+//	static int c;
+//	c = x + y;
+//	//c = x + y;
+//
+//	return c;
+//}
+//
+//
+//int main()
+//{
+//	int& ret = Add(1, 2);
+//	cout << "Add(1, 2) = " << ret << endl;
+//
+//	Add(3, 4);
+//	cout << "Add(1, 2) = " << ret << endl;
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int a = 10;
+//
+//	int& b = a;
+//
+//	int* ptr = &a;
+//	return 0;
+//}
+
+
