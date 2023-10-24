82a4d3812bd5b3e740f5f03f5338c076ab445d4e
tianhuo-tongren <1811334612@qq.com>
2023-10-24 12:17:51 +0800

    23_10_24 引用和指针

---
+++ test_10_24/test_10_24/Test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+//int main()
+//{
+//	int a = 10;
+//
+//	int& b = a;
+//
+//	int* c = &a;
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int& a1 = a;
+//
+//	cout << "&a = " << &a << endl;
+//	cout << "&a1 = " << &a1 << endl;
+//
+//	return 0;
+//}
+
+
+int main()
+{
+	int a = 10;
+
+	int& a1 = a;
+
+	int* pa = &a;
+
+	return 0;
+}
