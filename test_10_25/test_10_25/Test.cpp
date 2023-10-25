bf187ef4d1d0af9911d11f926f75ff2d5ec8e7e7
tianhuo-tongren <1811334612@qq.com>
2023-10-25 23:58:07 +0800

    23_10_25 构造函数

---
+++ test_10_25/test_10_25/Test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+//class Date
+//{
+//private:
+//	/*int _year = 10;
+//	int _month = 10;
+//	int _day = 10;*/
+//	int _year;
+//	int _month;
+//	int _day;
+//
+//public:
+//	/*Date(int year = 10, int month = 10, int day = 10)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}*/
+//	void Print()
+//	{
+//		cout << _year << " " << _month << " " << _day << endl;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	Date d1;
+//	d1.Print();
+//
+//	return 0;
+//}
+
+
