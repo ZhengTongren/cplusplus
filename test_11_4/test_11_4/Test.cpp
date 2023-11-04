e2e2738c5571b54a58e9c2a5c26bb656f55ecdcd
tianhuo-tongren <1811334612@qq.com>
2023-11-04 12:20:02 +0800

    23_11_4

---
+++ test_11_4/test_11_4/Test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//
+//class Date
+//{
+//public:
+//	Date(int year = 10, int month = 10, int day = 10)
+//		:_year(year)
+//		,_month(month)
+//		,_day(day)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//
+//int main()
+//{
+//	Date d1(2022, 10, 25);
+//	Date d2;
+//
+//	//d2(d1);
+//
+//	return 0;
+//}
+
+
