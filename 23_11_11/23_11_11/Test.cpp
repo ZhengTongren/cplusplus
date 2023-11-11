8d5d204781b256343c0540e53d2e0eaacb630cc6
tianhuo-tongren <1811334612@qq.com>
2023-11-11 19:33:02 +0800

    23_11_11

---
+++ 23_11_11/23_11_11/Test.cpp
@@ -0,0 +1,89 @@
+// #include <iostream>
+
+//class Date
+//{
+//public:
+//    //Date() // 1. 无参构造函数
+//    //{
+//    //    _year = 1;
+//    //    _month = 1;
+//    //    _day = 1;
+//    //}
+//
+//    //Date(int year, int month, int day) // 2. 带参构造函数
+//    //{
+//    //    _year = year;
+//    //    _month = month;
+//    //    _day = day;
+//    //}
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+
+//int main()
+//{
+//    Date d1; // 调用无参构造函数
+//    Date d2(2013, 2, 22); // 调用带参构造函数
+//    Date d3();
+//    return 0;
+//}
+
+
+//class Date
+//{
+//public:
+//    //Date() // 1. 无参构造函数
+//    //{
+//    //    _year = 1;
+//    //    _month = 1;
+//    //    _day = 1;
+//    //}
+//
+//    //Date(int year, int month, int day) // 2. 带参构造函数
+//    //{
+//    //    _year = year;
+//    //    _month = month;
+//    //    _day = day;
+//    //}
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+//
+//int main()
+//{
+//    Date d1;
+//    return 0;
+//}
+
+
+//class Date
+//{
+//public:
+//    Date()
+//    {
+//        _year = _month = _day = 2;
+//    }
+//
+//    Date(int year = 1, int month = 1, int day = 1) 
+//    {
+//        _year = year;
+//        _month = month;
+//        _day = day;
+//    }
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+//int main()
+//{
+//    Date d1;
+//    return 0;
+//}
