fe95bd2aa9e6f1ca308852338bb2da383ea3fd06
tianhuo-tongren <1811334612@qq.com>
2023-11-12 18:50:15 +0800

    23_11_12 拷贝构造

--- 23_11_12/23_11_12/Test.cpp
+++ 23_11_12/23_11_12/Test.cpp
@@ -8,46 +8,148 @@
 
 #include <iostream>
 using namespace std;
+#include <stdlib.h>
+
+//
+//class Date
+//{
+//public:
+//	Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	Date(Date& dd)
+//	{
+//		_year = dd._year;
+//		_month = dd._month;
+//		_day = dd._day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << " " << _month << " " << _day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//
+//void Func1(Date d)
+//{
+//	d.Print();
+//}
+//
+//int main()
+//{
+//	Date d1(2023, 1, 1);
+//	Func1(d1);
+//	// Date d2(d1);
+//	return 0;
+//}
 
 
-class Date
+class Stack
 {
 public:
-	Date(int year = 1, int month = 1, int day = 1)
+	Stack(int capacity = 3)
 	{
-		_year = year;
-		_month = month;
-		_day = day;
+		_a = (int*)malloc(sizeof(int) * capacity);
+		if (nullptr == _a)
+		{
+			perror("malloc");
+			exit(-1);
+		}
+		_top = 0;
+		_capacity = capacity;
 	}
 
-	Date(Date& dd)
+	Stack(const Stack& st)
 	{
-		_year = dd._year;
-		_month = dd._month;
-		_day = dd._day;
+		_a = (int*)malloc(sizeof(int) * st._capacity);
+		if (_a == nullptr)
+		{
+			perror("malloc");
+			exit(-1);
+		}
+		memcpy(_a, st._a, sizeof(int) * st._top);
+		_capacity = st._capacity;
+		_top = st._top;
 	}
 
-	void Print()
+	~Stack()
 	{
-		cout << _year << " " << _month << " " << _day;
+		free(_a);
+		_top = _capacity = 0;
+		_a = nullptr;
 	}
 
 private:
-	int _year;
-	int _month;
-	int _day;
+	int* _a;
+	int _top;
+	int _capacity;
 };
 
 
-void Func1(Date d)
+void Func(Stack st)
 {
-	d.Print();
+	//...
+	
 }
+
 
 int main()
 {
-	Date d1(2023, 1, 1);
-	Func1(d1);
-	// Date d2(d1);
+	Stack st1;
+
+	Func(st1);
 	return 0;
 }
+
+
+//class Date
+//{
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+
+
+//class Date
+//{
+//public:
+//    Date(int year = 1, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//    // ¿½±´¹¹Ôìº¯Êý
+//    Date(const Date d)
+//    {
+//        _year = d._year;
+//        _month = d._month;
+//        _day = d._day;
+//    }
+//private:
+//    int _year;
+//    int _month;
+//    int _day;
+//};
+//
+//
+//int main()
+//{
+//    Date d1;
+//    Date d2(d1);
+//
+//    return 0;
+//}
