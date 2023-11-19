82742d6efb5590f13138093c0d43b7f40065fd15
ZhengTongren <fourlongmarch@foxmail.com>
2023-11-19 16:29:00 +0800

    23_11_19 explicit

---
+++ 23-11-19/Test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	explicit Date(int year = 2023, int month = 1, int day = 1)
+//		:_year(year)
+//		,_month(month)
+//		,_day(day)
+//	{}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//
+//int main()
+//{
+//	Date d1 = 1;
+//
+//	return 0;
+//}
+
+
