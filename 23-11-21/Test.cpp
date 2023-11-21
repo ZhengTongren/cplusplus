22f215da5219dd93a7a7e60f4c051881500a56b5
ZhengTongren <fourlongmarch@foxmail.com>
2023-11-21 19:09:05 +0800

    23_11_21 OJ static实例，C++内存分布，new delete

--- 23-11-21/Test.cpp
+++ 23-11-21/Test.cpp
@@ -31,33 +31,53 @@ using namespace std;
 //    }
 //};
 
+//
+//struct ListNode
+//{
+//	int _val;
+//	ListNode* _next;
+//
+//	ListNode(int x = 0)
+//		:_val(x)
+//		,_next(nullptr)
+//	{}
+//};
+//
+//
+//int main()
+//{
+//	//int* p1 = new int(2);
+//	////cout << *p1 << endl;
+//	//int* p2 = new int[2]{ 1, 2 };
+//	//
+//
+//	ListNode* p3 = new ListNode[3]{ ListNode(1), ListNode(2), ListNode(3) };
+//	ListNode* p2 = new ListNode[3]{ 2, 2, 3 };
+//	//ListNode* p1 = new ListNode(1);
+//
+//	delete p2;
+//	delete p3;
+//
+//
+//	return 0;
+//}
 
-struct ListNode
-{
-	int _val;
-	ListNode* _next;
 
-	ListNode(int x = 0)
-		:_val(x)
-		,_next(nullptr)
-	{}
-};
-
-
-int main()
-{
-	//int* p1 = new int(2);
-	////cout << *p1 << endl;
-	//int* p2 = new int[2]{ 1, 2 };
-	//
-
-	ListNode* p3 = new ListNode[3]{ ListNode(1), ListNode(2), ListNode(3) };
-	ListNode* p2 = new ListNode[3]{ 2, 2, 3 };
-	//ListNode* p1 = new ListNode(1);
-
-	delete p2;
-	delete p3;
+//int globalVar = 1;
+//static int staticGlobalVar = 1;
+//
+//void Test()
+//{
+//	static int staticVar = 1;
+//	int localVar = 1;
+//	int num1[10] = { 1, 2, 3, 4 };
+//	char char2[] = "abcd";
+//	const char* pChar3 = "abcd";
+//
+//	int* ptr1 = (int*)malloc(sizeof(int) * 4);
+//	
+//	free(ptr1);
+//	
+//}
 
 
-	return 0;
-}
