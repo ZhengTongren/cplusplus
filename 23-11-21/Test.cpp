731a00b11ed7cf9f234b0860eeb97bf351814650
ZhengTongren <fourlongmarch@foxmail.com>
2023-11-21 16:46:38 +0800

    23_11_21 牛客OJ static实例

---
+++ 23-11-21/Test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+//class Count
+//{
+//    friend class Solution;
+//public:
+//    static int GetCount()
+//    {
+//        _count++;
+//        return _count;
+//    }
+//private:
+//    static int _count;
+//};
+//
+//int Count::_count = 0;
+//
+//class Solution {
+//public:
+//    int Sum_Solution(int n) {
+//        int sum = 0;
+//
+//        for (int i = 0; i < n; i++)
+//        {
+//            sum += Count::GetCount();
+//        }
+//
+//        return sum;
+//    }
+//};
+
+
