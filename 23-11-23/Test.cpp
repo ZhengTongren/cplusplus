f287cc99668b423f68754665155d11f1316b8214
ZhengTongren <fourlongmarch@foxmail.com>
2023-11-23 17:29:55 +0800

    23_11_23 delete释放（new）数组空间

---
+++ 23-11-23/Test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//int main()
+//{
+//	int* p1 = new int;
+//	// 注意区分p2和p3
+//	int* p2 = new int(10);// 对*p2进行初始化 10
+//	int* p3 = new int[10];// p3 指向一块40个字节的int类型空间
+//
+//	int* p4 = new int[10]{ 1, 2, 3 };// 对p4所指空间进行初始化
+//
+//	delete p1, p2;// 释放空间
+//	delete[] p3, p4;
+//
+//	return 0;
+//}
+
+
